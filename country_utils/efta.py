b49fea6cc8668874e3778277129b11b3976754a7
Gustaf Sjöberg <gs@distrop.com>
2011-10-28 00:58:59 +0200

    Added EFTA membership test.

---
+++ country_utils/efta.py
@@ -0,0 +1,22 @@
+MEMBER_STATES = (
+    'IS',
+    'LI',
+    'NO',
+    'CH',
+)
+
+def is_member(iso_code, member_states=MEMBER_STATES):
+    """Return a boolean indicating whether country represented by `iso_code`
+    is a member of EFTA.
+
+    >>> is_member('SE')
+    False
+    >>> is_member('IS')
+    True
+
+    """
+    return iso_code in member_states
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
