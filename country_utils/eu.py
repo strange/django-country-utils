269043ff4545493aefce0f872ffc556ac731efaa
Gustaf Sjöberg <gs@distrop.com>
2011-10-28 00:58:50 +0200

    Added EU membership test.

---
+++ country_utils/eu.py
@@ -0,0 +1,49 @@
+MEMBER_STATES = (
+    'AT',
+    'BE',
+    'BG',
+    'CY',
+    'CZ',
+    'DE',
+    'DK',
+    'EE',
+    'EL',
+    'ES',
+    'FI',
+    'FR',
+    'HU',
+    'IE',
+    'IT',
+    'LT',
+    'LU',
+    'LV',
+    'MT',
+    'NL',
+    'PL',
+    'PT',
+    'RO',
+    'SE',
+    'SI',
+    'SK',
+    'UK', 
+)
+
+def is_member(iso_code, member_states=MEMBER_STATES):
+    """Return a boolean indicating whether country represented by `iso_code`
+    is a member of the EU.
+
+    >>> is_member('SE')
+    True
+    >>> is_member(u'SE')
+    True
+    >>> is_member('US')
+    False
+    >>> is_member('JP')
+    False
+
+    """
+    return iso_code in member_states
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
